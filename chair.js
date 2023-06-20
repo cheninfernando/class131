c62e25e55d0357be42147b863033ed8b0b0e3729
cheninfernando <113739631+cheninfernando@users.noreply.github.com>
2023-06-19 18:02:13 -0700

    Add files via upload

---
+++ chair.js
@@ -0,0 +1,31 @@
+function setup(){
+    canvas=createCanvas(640,420)
+    canvas.center();
+    objectdetector=ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML="status:Detecting objects"
+}
+img="";
+status=""
+function preload(){
+    img= loadImage('img4.avif');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+   
+}
+
+function modelloaded(){
+    console.log("modelloaded")
+    status=true;
+    objectdetector.detect(img,gotresults);
+
+}
+
+function gotresults(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+
+}
